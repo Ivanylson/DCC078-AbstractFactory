77c7aee57dc0f4803c14cb5f5466f410afbeff45
IvanylsonHG <ivanylson.honorio.goncalves@ice.ufjf.br>
2022-10-07 02:47:49 -0300

    Adicionando o teste

--- src/test/java/ClienteTest.java
+++ src/test/java/ClienteTest.java
@@ -1,5 +1,32 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-
 class ClienteTest {
+    @Test
+    void deveSerEmpresaAlimentos(){
+        AbstractFactory factory = new EmpresaAlmentosMetricaLealdade();
+        Cliente cliente = new Cliente(factory);
+        assertEquals("Empresa de Alimentos", cliente.imprimeAssociacao());
+    }
 
+    @Test
+    void deveSerMetricaLealdadeCliente(){
+        AbstractFactory factory = new EmpresaAlmentosMetricaLealdade();
+        Cliente cliente = new Cliente(factory);
+        assertEquals("Metrica de Lealdade Cliente", cliente.imprimeSatisfacao());
+    }
+
+    @Test
+    void deveSerEmpresaSaude(){
+        AbstractFactory factory = new EmpresaSaudeSatisfacaoCliente();
+        Cliente cliente = new Cliente(factory);
+        assertEquals("Empresa de Saúde", cliente.imprimeAssociacao());
+    }
+
+    @Test
+    void deveSerSatifacaoCliente(){
+        AbstractFactory factory = new EmpresaSaudeSatisfacaoCliente();
+        Cliente cliente = new Cliente(factory);
+        assertEquals("Satisfação do Cliente", cliente.imprimeSatisfacao());
+    }
 }
